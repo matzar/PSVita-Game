8998aabad05ad7d536001aecdaba6bb10cc10735
matt360 <1502616@abertay.ac.uk>
2017-06-12 17:03:32 +0100

    inline free_camera dev
    refactoring

--- camera.h
+++ camera.h
@@ -8,15 +8,25 @@ public:
 	virtual void update() = 0;
 	inline virtual void moveForward(float dt) = 0;
 	inline virtual void moveBackwards(float dt) = 0;
+
 	inline virtual void moveUp(float dt) = 0;
 	inline virtual void moveDown(float dt) = 0;
+
 	inline virtual void moveSideLeft(float dt) = 0;
 	inline virtual void moveSideRight(float dt) = 0;
 
+	inline virtual float getYaw() = 0;
+	inline virtual void setYaw(float arg) = 0;
 	inline virtual void addYaw(float dt, float value) = 0;
 	inline virtual void subtractYaw(float dt, float value) = 0;
+
+	inline virtual float getPitch() = 0;
+	inline virtual void setPitch(float arg) = 0;
 	inline virtual void addPitch(float dt, float value) = 0;
 	inline virtual void subtractPitch(float dt, float value) = 0;
+
+	inline virtual float getRoll() = 0;
+	inline virtual void setRoll(float arg) = 0;
 	inline virtual void addRoll(float dt, float value) = 0;
 	inline virtual void subtractRoll(float dt, float value) = 0;
 
@@ -32,22 +42,14 @@ public:
 	inline virtual float getLookAtY() = 0;
 	inline virtual float getLookAtZ() = 0;
 	
+	inline virtual float getSideX() = 0;
+	inline virtual float getSideY() = 0;
+	inline virtual float getSideZ() = 0;
+
 	inline virtual float getUpX() = 0;
 	inline virtual float getUpY() = 0;
 	inline virtual float getUpZ() = 0;
 	
-	inline virtual float getSideX() = 0;
-	inline virtual float getSideY() = 0;
-	inline virtual float getSideZ() = 0;
-	
-	inline virtual float getYaw() = 0;
-	inline virtual float getPitch() = 0;
-	inline virtual float getRoll() = 0;
-
-	inline virtual void setYaw(float arg) = 0;
-	inline virtual void setPitch(float arg) = 0;
-	inline virtual void setRoll(float arg) = 0;
-
 	inline virtual void updateYaw(int width, int mouseX, int speed) = 0;
 	inline virtual void updatePitch(int height, int mouseY, int speed) = 0;
 	//virtual void cameraControll(float dt, int width, int height) const = 0;
--- free_camera.h
+++ free_camera.h
@@ -12,15 +12,25 @@ public:
 	virtual void update();
 	inline virtual void moveForward(float dt) { position += forward * dt; }
 	inline virtual void moveBackwards(float dt) { position -= forward * dt; }
+
 	inline virtual void moveUp(float dt) { position += up * dt; }
 	inline virtual void moveDown(float dt) { position -= up * dt; } 
+
 	inline virtual void moveSideLeft(float dt) { position -= side * dt; }
 	inline virtual void moveSideRight(float dt) { position += side * dt; }
 		   
+	inline virtual float getYaw() { return Yaw; }
+	inline virtual void setYaw(float arg) { Yaw = arg; }
 	inline virtual void addYaw(float dt, float value) { Yaw += value * dt; }
 	inline virtual void subtractYaw(float dt, float value) { Yaw -= value * dt; } 
+
+	inline virtual float getRoll() { return Roll; }
+	inline virtual void setPitch(float arg) { Pitch = arg; }
 	inline virtual void addPitch(float dt, float value) { Pitch += value * dt; } 
 	inline virtual void subtractPitch(float dt, float value) { Pitch -= value * dt; } 
+
+	inline virtual float getPitch() { return Pitch; }
+	inline virtual void setRoll(float arg) { Roll = arg; }
 	inline virtual void addRoll(float dt, float value) { Roll += value * dt; } 
 	inline virtual void subtractRoll(float dt, float value) { Roll -= value * dt; } 
 
@@ -36,24 +46,13 @@ public:
 	inline virtual float getLookAtY() { return lookAt.y(); } 
 	inline virtual float getLookAtZ() { return lookAt.z(); } 
 	
-	inline virtual float getUpX() { return up.x(); } 
-	inline virtual float getUpY() { return up.y(); } 
-	inline virtual float getUpZ() { return up.z(); }  
 	inline virtual float getSideX() { return side.x(); } 
 	inline virtual float getSideY() { return side.y(); } 
 	inline virtual float getSideZ() { return side.z(); } 
 
-	inline virtual float getYaw() { return Yaw; } 
-	inline virtual float getPitch() { return Pitch; } 
-	inline virtual float getRoll() { return Roll; }  
-
-	inline virtual void setLookAtX(float x) { lookAt.set_x(x); } 
-	inline virtual void setLookAtY(float y) { lookAt.set_y(y); } 
-	inline virtual void setLookAtZ(float z) { lookAt.set_z(z); } 
-
-	inline virtual void setYaw(float arg) { Yaw = arg; } 
-	inline virtual void setPitch(float arg) { Pitch = arg; } 
-	inline virtual void setRoll(float arg) { Roll = arg; }  
+	inline virtual float getUpX() { return up.x(); } 
+	inline virtual float getUpY() { return up.y(); } 
+	inline virtual float getUpZ() { return up.z(); }  
 
 	inline virtual void updateYaw(int width, int mouseX, int speed) { Yaw += static_cast<float>((mouseX - (width / 2)) / speed); } 
 	inline virtual void updatePitch(int height, int mouseY, int speed) { Pitch -= static_cast<float>((mouseY - (height / 2)) / speed); } 
