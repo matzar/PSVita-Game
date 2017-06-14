69aa1b4de28db7554f29d3eaea05dba3a123b356
matt360 <1502616@abertay.ac.uk>
2017-06-14 16:11:59 +0100

    gamestate machine working. Frame rate drop to 30fps

--- scene_app.cpp
+++ scene_app.cpp
@@ -529,14 +529,8 @@ void SceneApp::UpdateSimulation(float frame_time)
 	{
 		input_manager_->Update();
 
-		// get controller input data for all controllers
-		//gef::SonyControllerInputManager* controller_manager = input_manager_->controller_input();
 		// get controller input data and read controller data for controler 0
 		const gef::SonyController* controller = input_manager_->controller_input()->GetController(0);
-		//if (controller_manager)
-		//{
-		// read controller data for controler 0
-		//const gef::SonyController* controller = controller_manager->GetController(0);
 
 		if (controller)
 		{
@@ -600,71 +594,102 @@ void SceneApp::UpdateSimulation(float frame_time)
 			{
 				camera_->SetCameraPosition(90.0f, -12.0f, 0.0f, gef::Vector4(-8.0f, 3.5f, 0.0f));
 			}
-		} // controller
-		  //} // controller_manager
+
+			// trigger a sound effect
+			if (audio_manager_)
+			{
+				if (controller->buttons_pressed() & gef_SONY_CTRL_CIRCLE)
+					//if (cross)
+				{
+					if (sfx_voice_id_ == -1)
+					{
+						sfx_voice_id_ = audio_manager_->PlaySample(sfx_id_, true);
+
+						gef::VolumeInfo volume_info;
+						volume_info.volume = 0.5f;
+						volume_info.pan = -1.0f;
+
+						audio_manager_->SetSampleVoiceVolumeInfo(sfx_voice_id_, volume_info);
+
+						audio_manager_->SetSamplePitch(sfx_voice_id_, 1.5f);
+					}
+				}
+				if (controller->buttons_pressed() & gef_SONY_CTRL_TRIANGLE)
+				{
+					if (sfx_voice_id_ != -1)
+					{
+						audio_manager_->StopPlayingSampleVoice(sfx_voice_id_);
+						sfx_voice_id_ = -1;
+					}
+				}
+			} // audio_manager_
 
 #ifdef _WIN32 // Only on windows platforms
 		  // if there is a keyboard, check the arrow keys to control the direction of the character
-		gef::Keyboard* keyboard = input_manager_->keyboard();
-		if (keyboard)
-		{
-			//const gef::KeyboardD3D11* keyboard_d3d11 = (const gef::KeyboardD3D11*)keyboard;
-			float camera_speed = 10.0f;
-
-			// keyboard input
-			if (keyboard->IsKeyDown(gef::Keyboard::KC_W))
-				camera_->MoveForward(frame_time * camera_speed);
-			if (keyboard->IsKeyDown(gef::Keyboard::KC_S))
-				camera_->MoveBackwards(frame_time * camera_speed);
-			if (keyboard->IsKeyDown(gef::Keyboard::KC_A))
-				camera_->MoveSideLeft(frame_time * camera_speed);
-			if (keyboard->IsKeyDown(gef::Keyboard::KC_D))
-				camera_->MoveSideRight(frame_time * camera_speed);
-			if (keyboard->IsKeyDown(gef::Keyboard::KC_UP))
-				camera_->AddPitch(frame_time, camera_speed * camera_speed);
-			if (keyboard->IsKeyDown(gef::Keyboard::KC_DOWN))
-				camera_->subtractPitch(frame_time, camera_speed * camera_speed);
-			if (keyboard->IsKeyDown(gef::Keyboard::KC_LEFT))
-				camera_->subtractYaw(frame_time, camera_speed * camera_speed);
-			if (keyboard->IsKeyDown(gef::Keyboard::KC_RIGHT))
-				camera_->AddYaw(frame_time, camera_speed * camera_speed);
-			if (keyboard->IsKeyDown(gef::Keyboard::KC_R) || keyboard->IsKeyDown(gef::Keyboard::KC_NUMPAD8))
-				camera_->MoveUp(frame_time * camera_speed);
-			if (keyboard->IsKeyDown(gef::Keyboard::KC_F) || keyboard->IsKeyDown(gef::Keyboard::KC_NUMPAD2))
-				camera_->MoveDown(frame_time * camera_speed);
-		} // keyboard
-
-		  // mouse input
-		const gef::TouchInputManager* touch_input = input_manager_->touch_manager();
-		if (touch_input)
-		{
-			// initialise the mouse position
-			gef::Vector2 mouse_position(0.0f, 0.0f); // left upper corner of the window
-
-													 // get a pointer to the d3d11 implementation of the TouchInputManager
-			const gef::TouchInputManagerD3D11* touch_input_d3d11 = (const gef::TouchInputManagerD3D11*)touch_input;
-
-			// get the mouse position
-			mouse_position = touch_input_d3d11->mouse_position();
-			// camera's Yaw mouse controll, last variable controlls speed
-			//camera_->updateYaw(960, mouse_position.x, 2);
-			// camera's Pitch mouse controll, last variable controlls speed
-			//camera_->updatePitch(544, mouse_position.y, 2);
-
-			if (touch_input_d3d11->is_button_down(0))
+			gef::Keyboard* keyboard = input_manager_->keyboard();
+			if (keyboard)
 			{
-				//SetCursorPos(480, 272);	
-			}
+				//const gef::KeyboardD3D11* keyboard_d3d11 = (const gef::KeyboardD3D11*)keyboard;
+				float camera_speed = 10.0f;
 
-			//gef::DebugOut("Mouse position x, y: %f %f\n", mouse_position.x, mouse_position.y);
-		} // touch_input (mouse)
+				// keyboard input
+				if (keyboard->IsKeyDown(gef::Keyboard::KC_W))
+					camera_->MoveForward(frame_time * camera_speed);
+				if (keyboard->IsKeyDown(gef::Keyboard::KC_S))
+					camera_->MoveBackwards(frame_time * camera_speed);
+				if (keyboard->IsKeyDown(gef::Keyboard::KC_A))
+					camera_->MoveSideLeft(frame_time * camera_speed);
+				if (keyboard->IsKeyDown(gef::Keyboard::KC_D))
+					camera_->MoveSideRight(frame_time * camera_speed);
+				if (keyboard->IsKeyDown(gef::Keyboard::KC_UP))
+					camera_->AddPitch(frame_time, camera_speed * camera_speed);
+				if (keyboard->IsKeyDown(gef::Keyboard::KC_DOWN))
+					camera_->subtractPitch(frame_time, camera_speed * camera_speed);
+				if (keyboard->IsKeyDown(gef::Keyboard::KC_LEFT))
+					camera_->subtractYaw(frame_time, camera_speed * camera_speed);
+				if (keyboard->IsKeyDown(gef::Keyboard::KC_RIGHT))
+					camera_->AddYaw(frame_time, camera_speed * camera_speed);
+				if (keyboard->IsKeyDown(gef::Keyboard::KC_R) || keyboard->IsKeyDown(gef::Keyboard::KC_NUMPAD8))
+					camera_->MoveUp(frame_time * camera_speed);
+				if (keyboard->IsKeyDown(gef::Keyboard::KC_F) || keyboard->IsKeyDown(gef::Keyboard::KC_NUMPAD2))
+					camera_->MoveDown(frame_time * camera_speed);
+			} // keyboard
+
+			  // mouse input
+			const gef::TouchInputManager* touch_input = input_manager_->touch_manager();
+			if (touch_input)
+			{
+				// initialise the mouse position
+				gef::Vector2 mouse_position(0.0f, 0.0f); // left upper corner of the window
+
+														 // get a pointer to the d3d11 implementation of the TouchInputManager
+				const gef::TouchInputManagerD3D11* touch_input_d3d11 = (const gef::TouchInputManagerD3D11*)touch_input;
+
+				// get the mouse position
+				mouse_position = touch_input_d3d11->mouse_position();
+				// camera's Yaw mouse controll, last variable controlls speed
+				//camera_->updateYaw(960, mouse_position.x, 2);
+				// camera's Pitch mouse controll, last variable controlls speed
+				//camera_->updatePitch(544, mouse_position.y, 2);
+
+				if (touch_input_d3d11->is_button_down(0))
+				{
+					//SetCursorPos(480, 272);	
+				}
+
+				//gef::DebugOut("Mouse position x, y: %f %f\n", mouse_position.x, mouse_position.y);
+			} // touch_input (mouse)
 #endif // !_WIN32
-	} // input_manager_
+		} 
+		// controller
 
-	  //camera_->moveSideLeft(frame_time);
-	  //camera_->moveUp(frame_time);
-	  //camera_->update();
-}
+		//camera_->moveSideLeft(frame_time);
+		//camera_->moveUp(frame_time);
+		//camera_->update();
+	} 
+	// input_manager_
+} 
+// UpdateSimulation
 
 void SceneApp::FrontendInit()
 {
@@ -799,6 +824,9 @@ void SceneApp::GameRelease()
 
 void SceneApp::GameUpdate(float frame_time)
 {
+	// get controller input data and read controller data for controler 0
+	const gef::SonyController* controller = input_manager_->controller_input()->GetController(0);
+
 	// trigger a sound effect
 	if (audio_manager_)
 	{
@@ -831,16 +859,13 @@ void SceneApp::GameUpdate(float frame_time)
 
 	UpdateSimulation(frame_time);
 
-	//// get controller input data and read controller data for controler 0
-	//const gef::SonyController* controller = input_manager_->controller_input()->GetController(0);
+	if (controller->buttons_pressed() & gef_SONY_CTRL_START)
+	{
+		GameRelease();
 
-	//if (controller->buttons_pressed() & gef_SONY_CTRL_START)
-	//{
-	//	GameRelease();
-
-	//	game_state_ = FRONTEND;
-	//	FrontendInit();
-	//}
+		game_state_ = FRONTEND;
+		FrontendInit();
+	}
 }
 
 void SceneApp::GameRender()
