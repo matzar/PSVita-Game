f3be10f1ab6a88f00b8694236867549fd3ac6621
matt360 <1502616@abertay.ac.uk>
2017-06-26 14:31:27 +0100

    colision listener refactoring

--- contact_listener.cpp
+++ contact_listener.cpp
@@ -41,26 +41,26 @@ void ContactListener::BeginContact(b2Contact * contact)
 	// a pointer from the physics object to the entity in the game
 	void* bodyUserDataA = contact->GetFixtureA()->GetBody()->GetUserData();
 	// get body's A type
-	// we cast body's A user data to GameObject pointer to be able to access its type
-	uint16 typeA = ((GameObject*)(bodyUserDataA))->GetGameObjectType();
+	// we cast the bodyUserDataA void pointer to a pointer of the type GameObject to be able to access its type
+	OBJECT_TYPE typeA = ((GameObject*)(bodyUserDataA))->GetGameObjectType();
 
 	// a pointer from the physics object to the entity in the game
 	void* bodyUserDataB = contact->GetFixtureB()->GetBody()->GetUserData();
 	// get body's B type
-	// we cast body's B user data to GameObject pointer to be able to access its type
-	uint16 typeB = ((GameObject*)(bodyUserDataB))->GetGameObjectType();
+	// we cast the bodyUserDataB void pointer to a pointer of the type GameObject to be able to access its type
+	OBJECT_TYPE typeB = ((GameObject*)(bodyUserDataB))->GetGameObjectType();
 
 	// if body A is of the type PLAYER
 	if (typeA == PLAYER)
 	{
-		player = static_cast<Player*>(bodyUserDataA);
-		game_object = static_cast<GameObject*>(bodyUserDataB);
+		player = (Player*)(bodyUserDataA);
+		game_object = (GameObject*)(bodyUserDataB);
 	}
 	// if body B is of the type PLAYER
 	else if (typeB == PLAYER)
 	{
-		player = static_cast<Player*>(bodyUserDataB);
-		game_object = static_cast<GameObject*>(bodyUserDataA);
+		player = (Player*)(bodyUserDataB);
+		game_object = (GameObject*)(bodyUserDataA);
 	}
 
 	// collision response
@@ -68,47 +68,40 @@ void ContactListener::BeginContact(b2Contact * contact)
 	{
 		player->StartContact();
 
-		if (game_object->GetGameObjectType() == GROUND)
+		if (game_object)
 		{
-			current_ground_ = (Ground*)(game_object);
+			// get the current ground player is at
+			if (game_object->GetGameObjectType() == GROUND)
+			{
+				current_ground_ = (Ground*)(game_object);
+			}
+			// check if player is colliding with a pickup
+			if (game_object->GetGameObjectType() == PICKUP)
+			{
+				// schedule pickup for removal
+				dying_pickups_scheduled_for_removal_.insert(game_object);
+
+				gef::DebugOut("Pick this shit up!\n");
+			}
 		}
-		//
+		// check if player is of the same colour as the ground
 		if (player->GetGameObjectColour() == game_object->GetGameObjectColour())
 		{
 			// reset jump
 			player->RestartJump();
 			gef::DebugOut("Same colour\n");
 		}
-		//
-		if (game_object->GetGameObjectType() == GROUND && player->GetGameObjectColour() != game_object->GetGameObjectColour())
-		{
-			// destroy player
-			player->DeadPlayer();
 
-			// restart screen
-			gef::DebugOut("Different colours\n");
-		}
+		// check if the player is of the different colour than the ground
+		//if (game_object->GetGameObjectType() == GROUND && player->GetGameObjectColour() != game_object->GetGameObjectColour())
+		//{
+		//	// destroy player
+		//	player->DeadPlayer();
 
-		if (game_object)
-		{
-			//
-			if (game_object->GetGameObjectType() == PICKUP)
-			{
-				// set pickup to not active
-				dying_pickups_scheduled_for_removal_.insert(game_object);
-
-				gef::DebugOut("Pick this shit up!\n");
-			}
-		}
+		//	// restart screen
+		//	gef::DebugOut("Different colours\n");
+		//}
 	}
-
-	/*if (game_object)
-	{
-		if (game_object->GetGameObjectType() == GROUND)
-		{
-
-		}
-	}*/
 }
 
 void ContactListener::EndContact(b2Contact * contact)
@@ -120,13 +113,13 @@ void ContactListener::EndContact(b2Contact * contact)
 	// a pointer from the physics object to the entity in the game
 	void* bodyUserDataA = contact->GetFixtureA()->GetBody()->GetUserData();
 	// get body's A type
-	// we cast body's A user data to GameObject pointer to be able to access its type
+	// we cast the bodyUserDataA void pointer to a pointer of the type GameObject to be able to access its type
 	OBJECT_TYPE typeA = ((GameObject*)(bodyUserDataA))->GetGameObjectType();
 
 	// a pointer from the physics object to the entity in the game
 	void* bodyUserDataB = contact->GetFixtureB()->GetBody()->GetUserData();
 	// get body's B type
-	// we cast body's B user data to GameObject pointer to be able to access its type
+	// we cast the bodyUserDataB void pointer to a pointer of the type GameObject to be able to access its type
 	OBJECT_TYPE typeB = ((GameObject*)(bodyUserDataB))->GetGameObjectType();
 
 	// if body A is of the type PLAYER
